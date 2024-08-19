2fd2ce7e0df906ddf0cac3bc423ee5799f3cdc45
dudezi <a01036952858@gmail.com>
2024-08-19 12:08:56 +0900

    work

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -4,6 +4,6 @@ package org.example;
 // 에디터 여백에 있는 <icon src="AllIcons.Actions.Execute"/> 아이콘을 클릭하세요.
 public class Main {
     public static void main(String[] args) {
-        System.out.printf("Hi!!!");
+        System.out.printf("Hi!");
     }
 }
